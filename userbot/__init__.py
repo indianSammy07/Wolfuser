eec8b28f3ae608030fb8f3b0053bc4aee39da3d7
indianSammy07 <67373026+indianSammy07@users.noreply.github.com>
2020-06-25 11:47:53 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -113,7 +113,7 @@ if bool(ENV):
     MAX_FLOOD_IN_P_M_s = int(os.environ.get("MAX_FLOOD_IN_P_M_s", 5))
     UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/sandy1709/catuserbot.git")
+    "https://github.com/indianSammy07/WolfUserbot.git")
     
     # Time & Date - Country and Time Zone
     COUNTRY = str(os.environ.get("COUNTRY", ""))
