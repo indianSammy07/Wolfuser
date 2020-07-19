31280b1342b3ddb43c45b4d7a3deff9d0cefc286
MrSammy <67373026+indianSammy07@users.noreply.github.com>
2020-07-19 18:18:23 +0530

    Update whois.py

--- userbot/plugins/whois.py
+++ userbot/plugins/whois.py
@@ -263,19 +263,19 @@ async def fetch_info(replied_user, event):
         "This User has no Username")
     user_bio = "This User has no About" if not user_bio else user_bio
 
-    caption = "<b>USER INFO from cat's database :</b>\n\n"
-    caption += f"First Name: {first_name}\n"
-    caption += f"Last Name: {last_name}\n"
-    caption += f"Username: {username}\n"
-    caption += f"Data Centre ID: {dc_id}\n"
-    caption += f"Number of Profile Pics: {replied_user_profile_photos_count}\n"
-    caption += f"Is Bot: {is_bot}\n"
-    caption += f"Is Restricted: {restricted}\n"
-    caption += f"Is Verified by Telegram: {verified}\n"
-    caption += f"ID: <code>{user_id}</code>\n\n"
-    caption += f"Bio: \n<code>{user_bio}</code>\n\n"
-    caption += f"Common Chats with this user: {common_chat}\n"
-    caption += f"Permanent Link To Profile: "
+    caption = "<b>USER INFO FROM WOLF DATABASE :</b>\n\n"
+    caption += f"🧒First Name: {first_name}\n"
+    caption += f"🖊️Last Name: {last_name}\n"
+    caption += f"🏷️Username: {username}\n"
+    caption += f"🌍Data Centre ID: {dc_id}\n"
+    caption += f"🖼️Number of Profile Pics: {replied_user_profile_photos_count}\n"
+    caption += f"🤖Is Bot: {is_bot}\n"
+    caption += f"🔏Is Restricted: {restricted}\n"
+    caption += f"🌐Is Verified by Telegram: {verified}\n"
+    caption += f"🔖ID: <code>{user_id}</code>\n\n"
+    caption += f"✍️Bio: \n<code>{user_bio}</code>\n\n"
+    caption += f"👥Common Chats with this user: {common_chat}\n"
+    caption += f"📎Permanent Link To Profile: "
     caption += f"<a href=\"tg://user?id={user_id}\">{first_name}</a>"
 
     return photo, caption
