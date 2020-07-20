fea4f31f607d585b2ed94958fc851a1bbd483e80
Crackexy <67500032+Crackexy@users.noreply.github.com>
2020-07-20 13:19:36 +0530

    Fake Network Boost command.

---
+++ userbot/plugins/Networkfake.py
@@ -0,0 +1,61 @@
+"""Emoji
+
+Available Commands:
+
+.network"""
+
+from telethon import events
+
+import asyncio
+
+
+
+
+
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 1
+
+    animation_ttl = range(0, 19)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "network":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+        
+            "`Connecting To Sim Network...`",
+            "`█ ▇ ▆ ▅ ▄ ▂ ▁`",
+            "`▒ ▇ ▆ ▅ ▄ ▂ ▁`",
+            "`▒ ▒ ▆ ▅ ▄ ▂ ▁`",
+            "`▒ ▒ ▒ ▅ ▄ ▂ ▁`",    
+            "`▒ ▒ ▒ ▒ ▄ ▂ ▁`",
+            "`▒ ▒ ▒ ▒ ▒ ▂ ▁`",
+            "`▒ ▒ ▒ ▒ ▒ ▒ ▁`",
+            "`▒ ▒ ▒ ▒ ▒ ▒ ▒`",
+            "*Optimising Network...*",
+            "`▒ ▒ ▒ ▒ ▒ ▒ ▒`",
+            "`▁ ▒ ▒ ▒ ▒ ▒ ▒`",           
+            "`▁ ▂ ▒ ▒ ▒ ▒ ▒`",
+            "`▁ ▂ ▄ ▒ ▒ ▒ ▒`",
+            "`▁ ▂ ▄ ▅ ▒ ▒ ▒`",
+            "`▁ ▂ ▄ ▅ ▆ ▒ ▒`",
+            "`▁ ▂ ▄ ▅ ▆ ▇ ▒`",
+            "`▁ ▂ ▄ ▅ ▆ ▇ █`",
+            "**Sim Network Boosted....**"
+
+ ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 19])
